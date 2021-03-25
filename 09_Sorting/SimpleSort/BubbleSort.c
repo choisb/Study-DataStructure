c9eda04187fdb36bafe5522ba5705e2bb503dd6d
Seong Beom Choi <s004402@gmail.com>
2021-03-25 14:22:34 +0900

    Ch09_Sorting

--- 09_Sorting/SimpleSort/BubbleSort.c
+++ 09_Sorting/SimpleSort/BubbleSort.c
@@ -5,12 +5,12 @@ void BubbleSort(int arr[], int n)
 	int i, j;
 	int temp;
 
-	for (i = 0; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < n - 1; j++)
 		{
 			if (arr[j] > arr[j + 1]) {
-				// 데이터 교
+				// 데이터 교환
 				temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
